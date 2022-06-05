7729aa1af22fb04153e161a8795a3cb826ce5772
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2022-06-05 14:34:56 +0700

    Update .gitignore

--- news_info/lib/services/api_seach.dart
+++
@@ -1,8 +0,0 @@
-import 'base_network_cari.dart';
-
-class BeritaDataSource {
-  static BeritaDataSource instance = BeritaDataSource();
-  static Future<Map<String, dynamic>> loadCountries(String partUrl) async {
-    return BaseNetwork.get(partUrl);
-  }
-}
